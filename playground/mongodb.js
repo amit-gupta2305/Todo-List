fb739a2057e26dbe96da7e378109985429e4286c
Amit Gupta <amit.gupta2305@gmail.com>
2017-10-16 19:35:20 +0530

    Init Commit

---
+++ playground/mongodb.js
@@ -0,0 +1,22 @@
+//const mongo = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+   if (err){
+   return    console.log('unable to connect to mongodb server' + err);
+   }
+   console.log('Connected to Mongo DB');
+   db.collection('Todos').insertOne({
+     name:'mayur',
+     age:27,
+     locatoin:'jhansi'
+   },(err,res)=>{
+     if(err){
+       return console.log('Unable to insert the record ' + err);
+     }
+     console.log('record inserted successfully');
+     console.log(JSON.stringify(res.ops,undefined,4));
+   })
+
+
+   db.close();
+ })
