c267932205f69e4a5bb28d8799e5f47c33987ae7
Amit Gupta <amit.gupta2305@gmail.com>
2017-10-17 00:06:13 +0530

    Update demo

---
+++ playground/mongodb-update.js
@@ -0,0 +1,21 @@
+//const mongo = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+   if (err){
+   return    console.log('unable to connect to mongodb server' + err);
+   }
+   console.log('Connected to Mongo DB');
+
+   db.collection('Todos').findOneAndUpdate({_id:new ObjectID("59e4f9f4ed5e3867d73fc830")},
+   {
+     $set:{name:'Aamit'},
+     $inc:{age:1}
+   },{
+     returnOriginal:false
+   }).then((count)=>{
+     console.log(count);
+   })
+
+
+   db.close();
+ })
